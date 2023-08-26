f4dd9d396116cd73760a9365d7f0977cdfc2ea0a
Hüseyin Serhat Han <59025999+UsainSrht@users.noreply.github.com>
2023-08-26 16:44:25 +0300

    upload files

---
+++ src/main/java/me/usainsrht/enchgui/EnchGUI.java
@@ -0,0 +1,17 @@
+package me.usainsrht.enchgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnchGUI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
